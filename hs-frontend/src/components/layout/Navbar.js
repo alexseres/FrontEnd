d2eab4bc58eeed1582e8116b4bd1a912684a2d26
Alex Seres <lilaalex95>
2020-10-05 16:49:19 +0200

    Footer added

--- hs-frontend/src/components/layout/Navbar.js
+++ hs-frontend/src/components/layout/Navbar.js
@@ -1,15 +1,48 @@
 import React from "react";
-import styled from "styled-components"
-import {Link} from "react-router-dom"
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+import img from "./StoneHEarthHeader.png";
 
 
 const Navbar = () => {
-    <Header>
-        <Header></Header>
+    <Header style={{color: "red"}}>
+        <Title primary>HearthSTORM</Title>
+        <Link style={linkStyle} to="/">
+            Info{" "}
+        </Link>
+        <Link style={linkStyle} to={"/cards"}>
+            Cards
+        </Link>
+        <Link style={linkStyle} to={"/cards-back"}>
+            Cards Back
+        </Link>
+        <Link style={linkStyle} to={"/favourite-cards"}>
+            Favourite Cards
+        </Link>
+      
     </Header>
 }
 
 const Header = styled.header`
     border: 1px solid #000;
-    background-image
+    background-image: url(${img});
+    text-align: center;
+    padding: 15px;
+    font-family: "Comic Sans MS";
+    img{
+        max-width: 100%;
+        height: auto;
+    }
 `;
+
+const linkStyle = {
+    color: "white",
+    textDecoration: "none"
+};
+
+const Title = styled.h1`
+    font-family: "Comic Sans MS";
+    font-szie: 1em;
+    text-align: center;
+    color: blue;
+`
