f046bded98241c3d2600ece2b6845f72f2ffcfdf
Alex Seres <lilaalex95>
2020-10-05 17:47:04 +0200

    main app been created

--- hs-frontend/src/App.js
+++ hs-frontend/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Info from "./components/Info";
+import Cards from "./components/Cards";
+import BackCards from "./components/CardsBack";
 
 function App() {
   return (
-      
+      <div className="App">
+        <Router>
+          <Navbar/>
+          <Switch>
+            <Route exact path="/" component={Info}/>
+            <Route exact path="/cards" component={Cards}/>
+            <Route exact path="/cards-back"/>
+          </Switch>
+        </Router>
+      </div>
   );
 }
 
---
+++ hs-frontend/src/components/Cards.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Cards = () =>{
+    return (
+        <h2>Cards</h2>
+    )
+}
+
+export default Cards;
