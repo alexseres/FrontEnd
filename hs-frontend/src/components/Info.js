b4b53b85d138ad7498c2738c0cf18f9580885397
Alex Seres <lilaalex95>
2020-10-05 16:57:01 +0200

    Basic info page been added

---
+++ hs-frontend/src/components/Info.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+const Info = () => {
+    return (
+        <div>
+            <h1 style ={{textAlign: "center"}}>
+                Info
+            </h1>
+        </div>
+    )
+}
+
+export default Info;
