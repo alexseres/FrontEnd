fb5b80944c48018fff328cb7dd0e77f57b8edf82
HaveaKillerDay <45069029+HaveaKillerDay@users.noreply.github.com>
2020-10-08 12:17:21 +0200

    Modals are scrollable now

--- hs-frontend/src/components/cardbacks/OneBack.js
+++ hs-frontend/src/components/cardbacks/OneBack.js
@@ -1,60 +1,61 @@
 import React, { useEffect, useState, useContext } from "react";
 import { BackContext } from "./BackContext";
+import defaultImg from "../images/defaultCardIMG.png";
 import Modal from "./CardBackModal";
 
 const OneBack = (props) => {
+  const [isLoading, setLoading] = useState(true);
 
-    const [isLoading, setLoading] = useState(true);
+  const [toggleTheme, getTheme] = useContext(BackContext);
 
-    const [toggleTheme, getTheme] = useContext(BackContext);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
-    useEffect(() => {
-        setLoading(false);
-    }, [])
+  const handleCardClick = (event) => {
+    toggleTheme();
+  };
 
+  const handleError = (event) => {
+    event.target.src = props.card.img;
+  };
 
-    const handleCardClick = event => {
-        toggleTheme();
-    }
+  let content = (
+    <div>
+      <p>Loading</p>
+    </div>
+  );
 
-
-
-    let content = (
-        <div>
-            <p>Loading</p>
-        </div>
+  if (isLoading) {
+    content = (
+      <div className="card">
+        <p>Loading...</p>
+      </div>
     );
+  } else if (!isLoading) {
+    content = (
+      <div className="card" style={cardStyle} onClick={handleCardClick}>
+        <img
+          src={props.card.imgAnimated}
+          onError={defaultImg}
+          alt="Card Back"
+        ></img>
+        <Modal details={props.card} />
+      </div>
+    );
+  }
 
-    if (isLoading) {
-        content = (
-            <div className="card">
-                <p>Loading...</p>
-            </div>
-        );
-    } else if (!isLoading) {
-        content = (
-                <div className="card" style={cardStyle} onClick={handleCardClick}>
-                <img src={props.card.imgAnimated} onError={props.card.img} alt="Card Back"></img>
-                <Modal details={props.card} />
-                </div>
-        );
-    }
-
-
-    return content;
-}
-
-
-
-const cardStyle = {
-    transition: "0.3s",
-    width: "320px",
-    textAlign: "center",
-    marginBottom: "10px",
-    marginLeft: "10px",
-    marginRight: "10px",
-    margin: "0 auto",
+  return content;
 };
 
+const cardStyle = {
+  transition: "0.3s",
+  width: "320px",
+  textAlign: "center",
+  marginBottom: "10px",
+  marginLeft: "10px",
+  marginRight: "10px",
+  margin: "0 auto",
+};
 
 export default OneBack;
